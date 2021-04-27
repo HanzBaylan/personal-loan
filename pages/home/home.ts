1da7c23bf75cc1929876461553769355beb006fd
HanzBaylan <hanz.baylan191049@my.lsgh.edu.ph>
2021-04-27 08:26:53 +0800

    2 files modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -18,14 +18,10 @@ export class HomePage {
     this.InterestPayable = this.LoanAmount * (this.InterestRate / 100) * this.LoanTerm;
     this.InterestPayable = parseFloat (this.InterestPayable.toFixed(2));
 
-  calculateAmountPayable(); {
-    this.AmountPayable = (this.LoanAmount + this.InterestPayable * (this.InterestRate / 100) * this.LoanTerm);
+    this.AmountPayable = (this.LoanAmount + this.InterestPayable);
     this.AmountPayable = parseFloat(this.AmountPayable.toFixed(2));
-    }
     
-  calculateMonthlyPayable(); {
      this.MonthlyPayable = ((this.InterestPayable + this.AmountPayable * (this.InterestRate / 100) * this.LoanTerm) / this.LoanTerm)
     this.MonthlyPayable = parseFloat(this.MonthlyPayable.toFixed(2));
   }
   }
-}
